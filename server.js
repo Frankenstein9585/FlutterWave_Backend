d1c2832e4e998e0f759e73af0c68cab7a6a959c9
Donald Peters <donny9585@gmail.com>
2024-10-26 23:19:16 +0100

    Completed initial server setup

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
-console.log('Happy Developing ✨')
+require('dotenv').config();
+
+const express = require('express');
+const server = express();
+const PORT = process.env.PORT || 3000;
+const session = require('express-session');
+
+server.use(session({
+    secret: process.env.FLW_SECRET_KEY,
+    saveUninitialized: false,
+    resave: false,
+    cookie: { maxAge: 60 * 60 * 1000 },
+}));
+
+server.use(express.json());
+
+
+server.listen(PORT, () => {
+   console.log(`Server started on http://localhost:${PORT}`);
+});
